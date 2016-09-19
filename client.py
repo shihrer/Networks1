82e6962c8cb63f3ae5ef567e4d7db74a14488d72
Michael Shihrer <michael@wolfssl.com>
2016-09-19 16:47:13 -0600

    Initial client/server work.

--- client.py
+++ client.py
@@ -1,54 +1,68 @@
 import socket
 
-player = input("Enter player (1 or 2): ")
-p = int(player)
-xString = input("Enter x coordinate: ")
-x=int(xString)
-yString = input("Enter y coordinate: ")
-y=int(yString)
+# player = input("Enter player (1 or 2): ")
+# p = int(player)
+# xString = input("Enter x coordinate: ")
+# x=int(xString)
+# yString = input("Enter y coordinate: ")
+# y=int(yString)
+#
+# fire = "x=%s&y=%s"% (xString,yString)
+# print(fire)
+#
+# answer = "20"
+#
+# if p==1:
+#     file="eboard.txt"
+# else:
+#     file="eboard2.txt"
+#
+# with open(file) as my_file:
+#     eboard = my_file.readlines()
+#
+# if answer =="404 Not Found":
+#     print("out of bounds.")
+#     xString = input("Enter x coordinate: ")
+#     yString = input("Enter y coordinate: ")
+#
+# if answer=="410 Gone":
+#     print("spot already fired upon")
+#     xString = input("Enter x coordinate: ")
+#     yString = input("Enter y coordinate: ")
+#
+# if answer=="400 Bad Request":
+#     print("bad request")
+#     xString = input("Enter x coordinate: ")
+#     yString = input("Enter y coordinate: ")
+#
+# if answer[0]=="2":
+#     if answer[1]=="1":
+#         row = list(eboard[y])
+#         row[x] = "X"
+#         eboard[y] = ''.join(row)
+#         f = open(file, 'w')
+#         f.truncate()
+#         for x in eboard:
+#             f.write(x)
+#     else:
+#         row = list(eboard[y])
+#         row[x] = "~"
+#         eboard[y] = ''.join(row)
+#         f= open(file, 'w')
+#         f.truncate()
+#         for x in eboard:
+#             f.write(x)
 
-fire = "x=%s&y=%s"% (xString,yString)
-print(fire)
+# if __name__ == "__main__":
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.connect(('127.0.0.1', 5001))
+#     s.send('POST /?x=0&y=0 HTTP/1.1\n\n'.encode())
+#     # while True:
+#     resp = s.recv(1024)
+#     # if resp == "": break
+#     print(resp)
+#
+#     s.close()
 
-answer = "20"
-
-if p==1:
-    file="eboard.txt"
-else:
-    file="eboard2.txt"
-
-with open(file) as my_file:
-    eboard = my_file.readlines()
-
-if answer =="404 Not Found":
-    print("out of bounds.")
-    xString = input("Enter x coordinate: ")
-    yString = input("Enter y coordinate: ")
-
-if answer=="410 Gone":
-    print("spot already fired upon")
-    xString = input("Enter x coordinate: ")
-    yString = input("Enter y coordinate: ")
-
-if answer=="400 Bad Request":
-    print("bad request")
-    xString = input("Enter x coordinate: ")
-    yString = input("Enter y coordinate: ")
-
-if answer[0]=="2":
-    if answer[1]=="1":
-        row = list(eboard[y])
-        row[x] = "X"
-        eboard[y] = ''.join(row)
-        f = open(file, 'w')
-        f.truncate()
-        for x in eboard:
-            f.write(x)
-    else:
-        row = list(eboard[y])
-        row[x] = "~"
-        eboard[y] = ''.join(row)
-        f= open(file, 'w')
-        f.truncate()
-        for x in eboard:
-            f.write(x)
+import requests
+r = requests.post('http://127.0.0.1:5001', params={'x':0, 'y':0})
--- server.py
+++ server.py
@@ -1,104 +1,138 @@
-import socket
+import socket, sys, requests
+#
+# board = []
+# eboard = []
+# hit=0
+# sink='0'
+# c=5
+# b=4
+# r=3
+# s=3
+# d=2
+#
+#
+# with open('board.txt') as my_file:
+#     board = my_file.readlines()
+#
+# with open('eboard.txt') as my_file:
+#     eboard = my_file.readlines()
+#
+#
+#
+# x=0
+# y=1
+#
+# char = board[y][x]
+#
+# charlist = list(board[y])
+#
+# if x>9 or x<0 or y>9 or y<0:
+#     print("ERROR 404")
+#
+# if char=='~':
+#     print("ERROR 410")
+#
+# if char=='_':
+#     hit=0
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# if char=='C':
+#     hit=1
+#     c=c-1
+#     if c==0:
+#         sink='C'
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# if char=='B':
+#     hit=1
+#     b=b-1
+#     if b==0:
+#         sink='B'
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# if char=='R':
+#     hit=1
+#     r=r-1
+#     if r==0:
+#         sink='R'
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# if char=='S':
+#     hit=1
+#     s=s-1
+#     if s==0:
+#         sink='S'
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# if char=='D':
+#     hit=1
+#     d=d-1
+#     if d==0:
+#         sink='D'
+#     charlist[x] = '~'
+#     board[y] = ''.join(charlist)
+#     f = open("board.txt", 'w')
+#     f.truncate()
+#     for x in board:
+#         f.write(x)
+#
+# print(hit,sink)
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-board = []
-eboard = []
-hit=0
-sink='0'
-c=5
-b=4
-r=3
-s=3
-d=2
 
+class BattleshipHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pass
 
-with open('board.txt') as my_file:
-    board = my_file.readlines()
+    def do_POST(self):
+        pass
 
-with open('eboard.txt') as my_file:
-    eboard = my_file.readlines()
+if __name__ == "__main__":
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # try:
+    #     s.bind(('127.0.0.1', 5001))
+    # except socket.error as err:
+    #     print(err[1])
+    #     sys.exit()
+    #
+    # s.listen()
+    # while True:
+    #     conn, addr = s.accept()
+    #     print("Client connected from " + addr[0] + ':' + str(addr[1]))
+    #     resp = (conn.recv(1024)).strip()
+    #     print(resp.decode())
+    #     response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><head><title>test</title></head><body><h1>Test</h1></body></html>"
+    #     conn.send(response.encode())
+    #     conn.close()
 
+    try:
+        server = HTTPServer(('127.0.0.1', 5001), BattleshipHandler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.socket.close()
 
-
-x=0
-y=1
-
-char = board[y][x]
-
-charlist = list(board[y])
-
-if x>9 or x<0 or y>9 or y<0:
-    print("ERROR 404")
-
-if char=='~':
-    print("ERROR 410")
-
-if char=='_':
-    hit=0
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-if char=='C':
-    hit=1
-    c=c-1
-    if c==0:
-        sink='C'
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-if char=='B':
-    hit=1
-    b=b-1
-    if b==0:
-        sink='B'
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-if char=='R':
-    hit=1
-    r=r-1
-    if r==0:
-        sink='R'
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-if char=='S':
-    hit=1
-    s=s-1
-    if s==0:
-        sink='S'
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-if char=='D':
-    hit=1
-    d=d-1
-    if d==0:
-        sink='D'
-    charlist[x] = '~'
-    board[y] = ''.join(charlist)
-    f = open("board.txt", 'w')
-    f.truncate()
-    for x in board:
-        f.write(x)
-
-print(hit,sink)
